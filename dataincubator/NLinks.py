cffaee446c0eed6e3234687f5f1e41a9128b2daa
Grace Ou <grace.ou@berkeley.edu>
2015-07-15 13:26:33 -0700

    fully functional code

--- dataincubator/NLinks.py
+++ dataincubator/NLinks.py
@@ -3,9 +3,6 @@ Created on Jul 15, 2015
 
 @author: gogqou
 
-
-
-
 You have a chain with N links numbered 1 through N. 
 Every minute, you draw a random link from a bag, and connect it to any other consecutively-numbered link that you drew before. 
 For example, if you drew 4,1,5,7,3, you would end up with three subchains: 1,(3,4,5),7. 
@@ -18,33 +15,33 @@ import itertools as iter
 from operator import itemgetter
 import matplotlib.pyplot as plt
 def group_consec(inputList):
-    consecList = []
     ranges = []
-
     for k, g in iter.groupby(enumerate(inputList), lambda (i,x):i-x):
         group = map(itemgetter(1), g)
         ranges.append((group[0], group[-1]))
-    print 'ranges', ranges
-    print 'num links', len(ranges)
     return len(ranges)
 
-def Mdistr(repeats = 2):
+def Mdistr(repeats = 10000):
     N = 8
     max_links = np.zeros([repeats, 1])
     for i in range(0,repeats):
+        print 'iterating', i
         max_links[i] = Nlinks(N)
     #calc mean
-    
+    mean = np.mean(max_links)
+    #calc standard dev
+    stdev = np.std(max_links)
     #plot distribution
     
-    plt.plot(max_links)
-    plt.show()
-    #calc standard dev
+    #plt.hist(max_links)
+    #plt.show()
+    
+    print 'avg= ', mean, 'stdev = ', stdev
+    return mean, stdev
     
 def Nlinks(N=8):
     M = 0
     bagList = np.linspace(1, N, N)
-    print bagList
     linksList = []
     while len(bagList)>0:
         #randomly find index and move from "bag" to chain list
@@ -52,7 +49,6 @@ def Nlinks(N=8):
         linksList.append(bagList[index])
         linksList.sort()
         bagList = np.delete(bagList, index)
-        print linksList
         numLinks = group_consec(linksList)
         if numLinks>M:
             M = numLinks
