80b1cb927514e3d329ad95bc2e86411a6005d4ae
Grace Ou <grace.ou@berkeley.edu>
2015-07-15 13:17:33 -0700

    can order list arrange consec nums and count links graphing and calculating stats

--- dataincubator/NLinks.py
+++ dataincubator/NLinks.py
@@ -14,21 +14,53 @@ Let M be the maximum number of subchains in this process.
 
 '''
 import numpy as np
-def Nlinks():
+import itertools as iter
+from operator import itemgetter
+import matplotlib.pyplot as plt
+def group_consec(inputList):
+    consecList = []
+    ranges = []
+
+    for k, g in iter.groupby(enumerate(inputList), lambda (i,x):i-x):
+        group = map(itemgetter(1), g)
+        ranges.append((group[0], group[-1]))
+    print 'ranges', ranges
+    print 'num links', len(ranges)
+    return len(ranges)
+
+def Mdistr(repeats = 2):
     N = 8
+    max_links = np.zeros([repeats, 1])
+    for i in range(0,repeats):
+        max_links[i] = Nlinks(N)
+    #calc mean
     
-    repeats = 1000
+    #plot distribution
+    
+    plt.plot(max_links)
+    plt.show()
+    #calc standard dev
+    
+def Nlinks(N=8):
+    M = 0
     bagList = np.linspace(1, N, N)
     print bagList
     linksList = []
     while len(bagList)>0:
+        #randomly find index and move from "bag" to chain list
         index = np.random.randint(0, len(bagList))
         linksList.append(bagList[index])
+        linksList.sort()
         bagList = np.delete(bagList, index)
         print linksList
-        print bagList
-    return 1
+        numLinks = group_consec(linksList)
+        if numLinks>M:
+            M = numLinks
+        #order and form chains of consecutive integers in the linksList
+        #itertools
+        
+    return M
 
 
 if __name__ == '__main__':
-    Nlinks()
+    Mdistr()
